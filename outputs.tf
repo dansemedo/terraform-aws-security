c358e41c9a642b1fb483496a05aecac302e0d668
Daniel Semedo <34478209+dansemedo@users.noreply.github.com>
2025-08-11 15:36:16 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,16 @@
-# Outputs for VPC Module
+# Outputs for Security Module
 
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+output "app_security_group_id" {
+  description = "ID of the application security group"
+  value       = aws_security_group.app.id
 }
 
-output "vpc_cidr_block" {
-  description = "CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
+output "ec2_role_arn" {
+  description = "ARN of the EC2 IAM role"
+  value       = aws_iam_role.ec2_role.arn
 }
 
-output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
-}
-
-output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+output "ec2_instance_profile_name" {
+  description = "Name of the EC2 instance profile"
+  value       = aws_iam_instance_profile.ec2_profile.name
 }
